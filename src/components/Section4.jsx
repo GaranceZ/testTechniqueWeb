7ac14aff0421c32251e3b0a68909e512844a959c
Garance <76995069+GaranceZ@users.noreply.github.com>
2024-03-19 21:19:07 +0100

    Section 4 v.01 (Texte et fleches)

---
+++ src/components/Section4.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/Section4.css'
+
+const Section4 = () => {
+    return ( 
+        <>
+        <div className="MainContainerSection4">
+
+            <div className="TextArrowSection4">
+                <div className="TextSection4">
+                <h1>What Our <span>Students</span> Say About Us</h1>
+                </div>
+
+                <div className="ArrowSection4">
+                <i class='bx bx-chevron-left'></i>
+                <i class='bx bx-chevron-right' ></i>
+                </div>
+
+                <div className="CommentsContainer">
+                    <div className="Comment">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+     );
+}
+ 
+export default Section4;
