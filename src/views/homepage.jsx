e8133abe38261209e3a673c2b8e6faacc6d07542
Garance <76995069+GaranceZ@users.noreply.github.com>
2024-03-19 19:37:04 +0100

    Ajout de la section 2 et 3

--- src/views/homepage.jsx
+++ src/views/homepage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Section1 from '../components/Section1';
+import Section2 from '../components/Section2';
+import Section3 from '../components/Section3';
 
 const HomePage = () => {
     return ( 
         <>
         <Navbar/>
         <Section1/>
+        <Section2/>
+        <Section3/>
         </>
      );
 }
